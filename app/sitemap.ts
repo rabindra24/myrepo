2f4cb764edf8dd9b1c6f3ba0ce125e1b4672da00
rabin2403 <rabindra2402@gmail.com>
2024-01-18 09:57:29 +0530

    created about contact and services page

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,30 +1,30 @@
-// import { MetadataRoute } from "next";
+import { MetadataRoute } from "next";
 
-// export default function sitemap(): MetadataRoute.Sitemap {
-//   return [
-//     {
-//       url: "http://localhost:3000/",
-//       lastModified: new Date(),
-//       changeFrequency: "yearly",
-//       priority: 1,
-//     },
-//     {
-//       url: "http://localhost:3000/about",
-//       lastModified: new Date(),
-//       changeFrequency: "monthly",
-//       priority: 0.8,
-//     },
-//     {
-//       url: "http://localhost:3000/contact",
-//       lastModified: new Date(),
-//       changeFrequency: "weekly",
-//       priority: 0.5,
-//     },
-//     {
-//       url: "http://localhost:3000/services",
-//       lastModified: new Date(),
-//       changeFrequency: "weekly",
-//       priority: 0.5,
-//     },
-//   ];
-// }
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "http://localhost:3000/",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "http://localhost:3000/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "http://localhost:3000/contact",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "http://localhost:3000/services",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+  ];
+}
